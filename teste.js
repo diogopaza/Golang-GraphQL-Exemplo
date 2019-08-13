98bae4f1ec8d37a1b705dbcbfb4df20079620da0
diogopaza <diogopazacvel@gmail.com>
2019-08-13 15:31:27 -0300

    jsdom

--- teste.js
+++ teste.js
@@ -1,8 +1,36 @@
+const app = require('express')()
 const jsdom = require('jsdom')
 const {JSDOM} = jsdom
+const   { getTextContent }   = require('./utils/dom.utils')
 
-const url = `https://secure.tibia.com/community/?subtopic=characters&name=hue+proliferator`
+app.set('view engine','pug')
+
+app.get('/',(req,res) =>{
+    res.render('index')
+
+})
+
+app.get('/scraping',(req,res) =>{
+    const url = `http://localhost:8000/`
 
 JSDOM.fromURL(url)
-    .then(dom => console.log(dom))
+    .then(dom => {
+        const playerNameSelector = 'body > table > tbody > tr:nth-child(2) > td:nth-child(2)'
+                                   
+        //const player = dom.window.document.querySelector(playerNameSelector)
+        const player = getTextContent(dom, playerNameSelector)
+        console.log('Dados: ', player)
+       
+    })
+    .then(() =>{
+        res.write("<h1>Scraping</h1>")
+    } )
+})
+
+
+app.listen("8000", function(){
+    console.log("rodando")
+
+})
+
 
